55d00d1ca90e17dc04e9521b3b5410a1fd4156e4
S.Novachenko <s.novachenko@qiwi.com>
2023-07-13 21:48:44 +0600

    static files

--- routes/card.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-
-router.get('/', function(req, res, next) {
-    res.render('card', { title: 'Card' });
-});
-
-module.exports = router;
